c17b6487f27caf6dd19b6e6d008d97bfe34793fc
shanefieldj <shanefieldjacob@gmail.com>
2024-04-05 18:08:15 -0400

    added header

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+// Header.tsx
+
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>My App Header</h1>
+      {/* Add any additional header content or navigation links here */}
+    </header>
+  );
+};
+
+export default Header;
